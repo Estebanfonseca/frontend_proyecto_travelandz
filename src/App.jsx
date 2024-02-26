761b8da715050613486b9ee5e9125098f08fb354
ext-efonseca_Agro <estebanp57@hotmail.com>
2024-02-25 21:42:41 -0500

    cambio color bg

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='bg-purple-300'>
+      <div className='bg-orange-300'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
